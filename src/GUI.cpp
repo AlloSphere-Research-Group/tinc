c4dc7ba77e90d9befca54729f609a528f1c740d0
Andres Cabrera <mantaraya36@gmail.com>
2020-07-23 16:00:00 -0700

    Add functions to draw parameter spaces and dimensions through al::ParameterGUI

---
+++ src/GUI.cpp
@@ -0,0 +1,54 @@
+#include "tinc/GUI.hpp"
+
+#include "al/ui/al_ParameterGUI.hpp"
+
+namespace tinc {
+
+namespace gui {
+
+void drawControl(std::shared_ptr<tinc::ParameterSpaceDimension> dim) {
+  ImGui::PushID(dim.get());
+  if (dim->size() == 1) {
+    std::string dimensionText = dim->getName() + ":" + dim->getCurrentId();
+    ImGui::Text("%s", dimensionText.c_str());
+  } else if (dim->size() > 1) {
+    if (dim->type == ParameterSpaceDimension::MAPPED) {
+      int v = dim->getCurrentIndex();
+      if (ImGui::SliderInt(dim->getName().c_str(), &v, 0, dim->size())) {
+        dim->setCurrentIndex(v);
+      }
+      ImGui::SameLine();
+      ImGui::Text("%s", dim->getCurrentId().c_str());
+    } else if (dim->type == ParameterSpaceDimension::INDEX) {
+      int v = dim->getCurrentIndex();
+      if (ImGui::SliderInt(dim->getName().c_str(), &v, 0, dim->size())) {
+        dim->setCurrentIndex(v);
+      }
+      ImGui::SameLine();
+      ImGui::Text("%s", dim->getCurrentId().c_str());
+    } else if (dim->type == ParameterSpaceDimension::INTERNAL) {
+      al::ParameterGUI::drawParameter(&dim->parameter());
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("-")) {
+      dim->stepDecrease();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("+")) {
+      dim->stepIncrement();
+    }
+  } else {
+    // do nothing
+  }
+  ImGui::PopID();
+}
+
+void drawControls(ParameterSpace &ps) {
+  for (auto dim : ps.dimensions) {
+    drawControl(dim);
+  }
+}
+
+} // namespace gui
+
+} // namespace tinc
