877bad3f0372752fcc39446ec54da27782100d92
Kon Hyong Kim <konhyong@gmail.com>
2020-10-07 15:52:07 -0700

    test binary written to CMAKE_SOURCE_DIR/bin, diskbuffer example updated

--- tests/tincprotocol.cpp
+++ tests/tincprotocol.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 
-#include "tinc/TincServer.hpp"
 #include "tinc/TincClient.hpp"
+#include "tinc/TincServer.hpp"
 
 #include "al/system/al_Time.hpp"
 using namespace tinc;
@@ -13,12 +13,11 @@ TEST(TincProtocol, Connection) {
   TincClient tclient;
   EXPECT_TRUE(tclient.start());
 
-  al::al_sleep(1.0); // Give time to connect
   EXPECT_EQ(tserver.connectionCount(), 1);
   EXPECT_TRUE(tclient.isConnected());
 
-  tserver.stop();
   tclient.stop();
+  tserver.stop();
 }
 
 TEST(TincProtocol, MultiConnection) {
@@ -29,28 +28,24 @@ TEST(TincProtocol, MultiConnection) {
   TincClient tclient;
   EXPECT_TRUE(tclient.start());
 
-  al::al_sleep(0.3); // This should be removed
   TincClient tclient2;
   EXPECT_TRUE(tclient2.start());
 
-  al::al_sleep(0.3); // This should be removed
   TincClient tclient3;
   EXPECT_TRUE(tclient3.start());
 
-  al::al_sleep(0.3); // This should be removed
   TincClient tclient4;
   EXPECT_TRUE(tclient4.start());
 
-  al::al_sleep(1.0); // Give time to connect
   EXPECT_EQ(tserver.connectionCount(), 4);
   EXPECT_TRUE(tclient.isConnected());
   EXPECT_TRUE(tclient2.isConnected());
   EXPECT_TRUE(tclient3.isConnected());
   EXPECT_TRUE(tclient4.isConnected());
 
-  tserver.stop();
   tclient.stop();
   tclient2.stop();
   tclient3.stop();
   tclient4.stop();
+  tserver.stop();
 }
