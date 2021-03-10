a0af9fdd7424502cc4b58318c537b0b2f78f27df
Andres Cabrera <mantaraya36@gmail.com>
2021-03-09 16:07:02 -0800

    Add diskbuffer unit test.

---
+++ tests/diskbuffer.cpp
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+
+#include "tinc/DiskBufferJson.hpp"
+
+using namespace tinc;
+
+TEST(DiskBuffer, BasicJson) {
+  DiskBufferJson db{"id", "name.ext", "path"};
+
+  db.enableRoundRobin(5);
+  auto filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_0.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_1.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_2.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_3.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_4.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_0.ext");
+};
+
+TEST(DiskBuffer, RoundRobin) {
+  DiskBufferJson db{"id", "name.ext", "path"};
+
+  db.enableRoundRobin(5);
+  auto filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_0.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_1.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_2.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_3.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_4.ext");
+  filename = db.getFilenameForWriting();
+  EXPECT_EQ(filename, "name_0.ext");
+};
